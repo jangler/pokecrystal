07e5275ec570cf5bdefc50a171e7badded0bc55b
Brandon Mulcahy <brandon@jangler.info>
2016-05-20 22:04:10 -0400

    Remove vending machines

--- fate-readme.txt
+++ fate-readme.txt
@@ -1,12 +1,22 @@
 Pokémon Crystal Fate
 ====================
 
-This ROM hack of Pokémon Crystal does not alter the content of the game, but
-alters many of the game mechanics in order to challenge the player.
+This ROM hack of Pokémon Crystal does not alter the core content of the game,
+but alters many of the game mechanics in order to challenge the player.
+
+Evolution and experience
+------------------------
 
 - Upon reaching every tenth level, a Pokémon evolves into another random
   species. Evolution cannot be canceled.
+- Experience gain is adjusted by the level differential between the player's
+  Pokémon and the fainting Pokémon. An overleveled Pokémon gains less
+  experience; an underdog gains more.
 - All species level up at the same rate.
+
+Healing
+-------
+
 - All full-party heals such as Pokémon Centers have been removed, except for
   Sacred Ash.
 - Revives, Max Revives, and Revival Herbs cannot be purchased and do not appear
@@ -16,5 +26,9 @@ alters many of the game mechanics in order to challenge the player.
 - Other healing and status-curing items can be purchased at approximately one
   half of their regular prices.
 - The Goldenrod City vending machines are out of service.
+
+Other
+-----
+
 - Repels have been removed, but using an even-numbered text box frame prevents
   random encounters.
--- maps/GoldenrodDeptStore6F.asm
+++ maps/GoldenrodDeptStore6F.asm
@@ -12,77 +12,8 @@ GoldenrodDeptStore6F_MapScriptHeader:
 GoldenrodVendingMachine:
 	opentext
 	writetext GoldenrodVendingText
-.Start
-	special PlaceMoneyTopRight
-	loadmenudata .MenuData
-	verticalmenu
-	closewindow
-	if_equal $1, .FreshWater
-	if_equal $2, .SodaPop
-	if_equal $3, .Lemonade
 	closetext
 	end
-
-.FreshWater
-	checkmoney $0, 200
-	if_equal $2, .NotEnoughMoney
-	giveitem FRESH_WATER
-	iffalse .NotEnoughSpace
-	takemoney $0, 200
-	itemtotext FRESH_WATER, $0
-	jump .VendItem
-
-.SodaPop
-	checkmoney $0, 300
-	if_equal $2, .NotEnoughMoney
-	giveitem SODA_POP
-	iffalse .NotEnoughSpace
-	takemoney $0, 300
-	itemtotext SODA_POP, $0
-	jump .VendItem
-
-.Lemonade
-	checkmoney $0, 350
-	if_equal $2, .NotEnoughMoney
-	giveitem LEMONADE
-	iffalse .NotEnoughSpace
-	takemoney $0, 350
-	itemtotext LEMONADE, $0
-	jump .VendItem
-
-.VendItem
-	pause 10
-	playsound SFX_ENTER_DOOR
-	writetext GoldenrodClangText
-	buttonsound
-	itemnotify
-	jump .Start
-
-.NotEnoughMoney
-	writetext GoldenrodVendingNoMoneyText
-	waitbutton
-	jump .Start
-
-.NotEnoughSpace
-	writetext GoldenrodVendingNoSpaceText
-	waitbutton
-	jump .Start
-
-.MenuData
-	db $40 ; flags
-	db 02, 00 ; start coords
-	db 11, 19 ; end coords
-	dw .MenuData2
-	db 1 ; default option
-
-.MenuData2
-	db $80 ; flags
-	db 4 ; items
-	db "FRESH WATER  ¥200@"
-	db "SODA POP     ¥300@"
-	db "LEMONADE     ¥350@"
-	db "CANCEL@"
-
 
 LassScript_0x564bf:
 	jumptextfaceplayer UnknownText_0x5654b
@@ -98,25 +29,7 @@ GoldenrodDeptStore6FElevatorButton:
 
 GoldenrodVendingText:
 	text "A vending machine!"
-	line "Here's the menu."
-	done
-
-GoldenrodClangText:
-	text "Clang! A can of"
-	line "@"
-	text_from_ram StringBuffer3
-	text $55
-	db "popped out!"
-	done
-
-GoldenrodVendingNoMoneyText:
-	text "Oops, not enough"
-	line "money."
-	done
-
-GoldenrodVendingNoSpaceText:
-	text "There's no more"
-	line "room for stuff."
+	line "It's out of order."
 	done
 
 UnknownText_0x5654b:
