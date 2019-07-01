1f1166e4ba9ad8d9d6873903a27c7419b58999c4
Brandon Mulcahy <brandon@lightcones.net>
2019-07-01 13:54:44 -0400

    Add +1 to level modifier again lol

--- home.asm
+++ home.asm
@@ -217,7 +217,7 @@ INCLUDE "home/sprite_anims.asm"
 INCLUDE "home/audio.asm"
 INCLUDE "home/mobile.asm"
 
-; add badge count to a.
+; add badge count + 1 to a.
 AddLevelModifier::
 	push bc
 	ld b, a
@@ -230,5 +230,6 @@ AddLevelModifier::
 	pop hl
 	ld a, [wNumSetBits]
 	add b
+	inc a
 	pop bc
 	ret
